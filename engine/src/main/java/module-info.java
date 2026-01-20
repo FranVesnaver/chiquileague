cb5cbc97ec28823568a59d7020969367b4835280
FranVesnaver <fevesnaver@gmail.com>
2026-01-20 00:49:24 -0300

    delete module-info

--- engine/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.chiquileague {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-    exports org.chiquileague.app;
-    opens org.chiquileague.app to javafx.fxml;
-}
